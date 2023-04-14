7e7328f42f652e1d88454f09722299d6f489d1e3
O_Ukandu <100975029+Ukandu20@users.noreply.github.com>
2023-04-13 21:46:19 -0400

    working on the login form

--- php/login.php
+++ php/login.php
@@ -2,7 +2,7 @@
 // Replace these with your own database credentials
 $servername = "localhost";
 $username = "ukandu_db";
-$password = "1234";
+$password = "";
 $dbname = "ukandu_db";
 
 // Create connection
@@ -13,28 +13,49 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+// Handle form submissions
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['login'])) {
+        // Handle login
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-$sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
-$result = $conn->query($sql);
+        $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+        $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // Login successful
-    $row = $result->fetch_assoc();
-    $role = $row['role'];
-    
-    if ($role == 'admin') {
-        // Redirect to admin user page
-        header("Location: admin_user_page.html");
-    } else {
-        // Redirect to authenticated user page
-        header("Location: authenticated_user_page.html");
+        if ($result->num_rows > 0) {
+            // Login successful
+            $row = $result->fetch_assoc();
+            $role = $row['role'];
+
+            if ($role == 'admin') {
+                // Redirect to admin user page
+                header("Location: admin_user_page.html");
+            } else {
+                // Redirect to authenticated user page
+                header("Location: authenticated_user_page.html");
+            }
+        } else {
+            // Login failed, redirect back to login page with an error message
+            header("Location: login.html?error=Invalid email or password");
+        }
+    } elseif (isset($_POST['signup'])) {
+        // Handle sign-up
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+        if ($conn->query($sql) === TRUE) {
+            // Sign-up successful, redirect to login page
+            header("Location: login.html?success=Account created successfully. You can now login");
+        } else {
+            // Sign-up failed, redirect back to sign-up page with an error message
+            header("Location: login.html?error=Error creating account. Please try again");
+        }
     }
-} else {
-    // Login failed, redirect back to login page with an error message
-    header("Location: login.html?error=Invalid email or password");
 }
 
 $conn->close();
-
+?>
